9531b32b38339fbcde1ecc98e534ee1815ffd0c5
Clement benony <clement.benony@etu-bordeaux.fr>
2025-04-14 15:02:10 +0200

    Ajout de la page d'accueil et de la navbar

---
+++ src/components/accueil.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Accueil: React.FC = () => {
+    return (
+        <div className="flex flex-col min-h-screen bg-slate-800 ">
+        <h1 className="text-3xl text-slate-100 font-bold text-center mt-8">
+          Bienvenue sur InfoBaïnes !
+        </h1>
+        <div className="flex flex-1 items-satrt justify-center p-15">
+          <p className='text-2xl text-center text-slate-100 mx-auto max-w-4xl px-4'>InfoBaïne est une application qui utilise une modélisation semi-empirique de la 
+            prévision de l'aléa de la houle, des courants d'arrachement et de la houle des vagues de bords
+            <br />
+            Vous y retrouverez differentes plages de la côte landaise.  
+          </p>
+        </div>
+      </div>
+    );
+};
+export default Accueil;
---
+++ src/components/navBar.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const NavBar: React.FC = () => {
+    return (
+        <div className="navbar bg-base-100 shadow-sm">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /> </svg>
+                    </div>
+                    <ul
+                        tabIndex={0}
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                        <li><a href="/">Accueil</a></li>
+                        <li><a href="/">La lette Blanche</a></li>
+                        <li><a href="/">Biscarosse</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div className="navbar-center">
+                <a href="/" className="btn btn-ghost text-xl">Info Baïnes</a>
+            </div>
+            <div className="navbar-end">
+                <a href="/" className="btn btn-ghost btn-circle">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+                    </svg>
+                </a>
+            </div>
+        </div>
+    );
+};
+
+export default NavBar;
