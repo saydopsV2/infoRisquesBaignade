a73c15dfd4df92e20e5f56f3a117577b68647ac5
Clement benony <clement.benony@etu-bordeaux.fr>
2025-04-23 15:58:27 +0200

    Responsive NaavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,7 +7,7 @@ const NavBar: React.FC = () => {
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="black"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /> </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 sm:h-5 sm:w-5" fill="none" viewBox="0 0 24 24" stroke="black"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /> </svg>
                     </div>
                     <ul
                         tabIndex={0}
@@ -25,7 +25,7 @@ const NavBar: React.FC = () => {
             </div>
             <div className="navbar-end">
                 <Link to="/" className="btn btn-ghost btn-circle">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="black">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 sm:h-5 sm:w-5" fill="none" viewBox="0 0 24 24" stroke="black">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
                     </svg>
                 </Link>
