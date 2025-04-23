3a9373bb3bc7bdaafac3d31dc94b2f76f83ac1b7
Clement benony <clement.benony@etu-bordeaux.fr>
2025-04-23 15:55:17 +0200

    Changement HeadBand

--- src/components/HeadBand.tsx
+++ src/components/HeadBand.tsx
@@ -1,8 +1,8 @@
 const HeadBand = () => {
   return (
     <div className="bg-yellow-50 shadow-md py-4 px-6 rounded-lg my-4 mx-auto max-w-7xl">
-      <div className="text-center mb-2 text-sm text-slate-600 font-medium">
-        Nos partenaires
+      <div className="text-center mb-2 text-lg text-slate-600 font-medium">
+        Projet soutenu par :
       </div>
       <div className="flex flex-wrap items-center justify-center gap-6 md:gap-10">
         <a 
